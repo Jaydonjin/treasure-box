8f1c836116d6de22d33cc05275585fe841a60ef3
Jaydon.T.Jin (g-mis.cncd02.Newegg) 42063 <jaydon.t.jin@newegg.com>
2018-12-27 17:55:59 +0800

    fix

--- config.js
+++ config.js
@@ -1,26 +1,26 @@
 const source = {
 	"prizes": [{
-			"text1": "500RMB * 8",
-			"text2": ""
+			"text1": "500RMB ",
+			"text2": "数量：8"
 		}, {
 			"text1": "翻2倍",
 			"text2": "加油打气"
 		},
 		{
-			"text1": "1000RMB * 5",
-			"text2": ""
+			"text1": "1000RMB",
+			"text2": "数量：5"
 		},
     {
     	"text1": "翻3倍",
     	"text2": "指压板跳跳"
     },
     {
-    	"text1": "500RMB * 8",
-    	"text2": ""
+    	"text1": "500RMB",
+    	"text2": "数量：8"
     },
     {
-    	"text1": "800RMB * 8",
-    	"text2": ""
+    	"text1": "800RMB",
+    	"text2": "数量：8"
     },
     {
     	"text1": "翻4倍",
